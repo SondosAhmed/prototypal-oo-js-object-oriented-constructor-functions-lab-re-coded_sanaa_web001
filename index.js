1a6b1bd8de3675ca63d58befd6a8f25e8113153c
Sondos Ahmed <sondosalshami@gmail.com>
2020-08-19 09:48:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,13 @@ function Scooter(year, color, model) {
     this.color=color;
     this.model=model;
 }
+function driver(year, color, model) {
+    this.year = year;
+    this.color=color;
+    this.model=model;
+}
+function PickupLocation(year, color, model) {
+    this.year = year;
+    this.color=color;
+    this.model=model;
+}
